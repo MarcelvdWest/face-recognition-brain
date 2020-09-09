fd8017b507460a361fe02eb79b667267782d95ba
MarcelvdWest <59703081+MarcelvdWest@users.noreply.github.com>
2020-09-09 15:54:20 +0200

    Updates

---
+++ precache-manifest.34d7cc8c066607a8bed0e6da7219098f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f12695910e31b31998bdb700c23af3",
+    "url": "/face-recognition-brain/index.html"
+  },
+  {
+    "revision": "8212f8d002db29d2c684",
+    "url": "/face-recognition-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ed3a6d1daf32962c567c",
+    "url": "/face-recognition-brain/static/css/main.2d39aa71.chunk.css"
+  },
+  {
+    "revision": "8212f8d002db29d2c684",
+    "url": "/face-recognition-brain/static/js/2.1729a7bf.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition-brain/static/js/2.1729a7bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3a6d1daf32962c567c",
+    "url": "/face-recognition-brain/static/js/main.c52f9732.chunk.js"
+  },
+  {
+    "revision": "56148025b8b7094fd937",
+    "url": "/face-recognition-brain/static/js/runtime-main.fa010191.js"
+  }
+]);
